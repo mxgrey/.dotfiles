b8fe42e63add6da0723cc4c7f43b9f99f77fbc4a
Michael X. Grey <grey@openrobotics.org>
2022-12-06 18:53:34 +0800

    Use a bare repo
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function dotfiles --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME' --description 'alias config /usr/bin/git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME'
-  /usr/bin/git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME $argv;
+function dotfiles --wraps='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' --description 'alias config /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv;
 end
